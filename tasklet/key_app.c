1aa02d87bc20189aefbfab7d6d72016155e6b6b1
wanyisq <wanyisq@163.com>
2021-03-20 23:32:54 +0800

    init tasklet files

---
+++ tasklet/key_app.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+#include "linux/ioctl.h"
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    int ret = 0;
+    char *filename;
+    unsigned char data;
+
+    if(argc != 2){
+        printf("error usage\r\n");
+        return -1;
+    }
+
+    filename = argv[1];
+    fd = open(filename, O_RDWR);
+    if(fd < 0){
+        printf("can not open file %s\r\n", filename);
+        return -1;
+    }
+    printf("open file %s successed\r\n", filename);
+
+    while(1){
+        ret = read(fd, &data, sizeof(data));
+        if(ret < 0){
+
+        }else{
+            if(data){
+                printf("key value %X\r\n", data);
+            }
+        }
+    }
+
+    close(fd);
+    return ret;
+}
